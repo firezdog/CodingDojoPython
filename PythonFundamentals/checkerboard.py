c17704b14208da2ddcbb957e820295ca971de958
firezdog@yahoo.com <firezdog@yahoo.com>
2018-03-04 20:28:22 -0600

    Updated checkerboard.py to avoid using pre-formatted strings.

--- PythonFundamentals/checkerboard.py
+++ PythonFundamentals/checkerboard.py
@@ -1,11 +1,19 @@
 #Assignment: produce a "checkerboard pattern" in the console using a while loop.
-
-string1 = "* * * *"
-string2 = " * * * *"
+string = ""
 i = 0
 while i < 8:
     if i%2 == 0:
-        print string1
+        j = 0
+        while j < 8:
+            string += "* "
+            j += 1
+        print string
+        string = ""
     else:
-        print string2
+        j = 0
+        while j < 8:
+            string += " *"
+            j += 1
+        print string
+        string = ""
     i += 1
